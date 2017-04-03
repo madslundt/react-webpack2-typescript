f847125404e0aa6a608dd926f6d9316b0df08764
Mads Lundt <madslundt@live.dk>
2017-04-03 10:41:47 +0200

    Added todo component

---
+++ src/Todo/Input/Input.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import TextField from 'material-ui/TextField';
+
+interface TodoInputProperty {
+    onSubmit(title: string): void;
+}
+
+interface TodoInputState {
+    value: string;
+}
+
+export class Input extends React.Component <TodoInputProperty, TodoInputState> {
+    constructor() {
+        super();
+        this.state = {
+            value: ''
+        };
+    }
+
+    private submitValue() {
+        this
+            .props
+            .onSubmit(this.state.value);
+        this.setState({value: ''});
+    }
+
+    onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (!this.state.value) {
+            return;
+        }
+
+        switch (event.keyCode) {
+            case 13: // enter
+                this.submitValue();
+        }
+    }
+
+    onChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {
+        return (
+            <TextField
+                type="text"
+                name="todoInput"
+                value={this.state.value}
+                onKeyDown={this.onKeyDown.bind(this)}
+                onChange={this.onChange.bind(this)}
+            />
+        );
+    }
+}
